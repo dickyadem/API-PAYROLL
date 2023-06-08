130546133fa5b11cdf0f3295749a16da81ad52f3
dicky ade mahendra <dickyadem@gmail.com>
2023-06-08 08:54:54 +0700

    tertinggal

--- app.js
+++ app.js
@@ -13,6 +13,5 @@ app.use("/pendapatan", require("./apps/pendapatan/PendapatanControllers"));
 app.use("/karyawan", require("./apps/karyawan/KaryawanControllers"));
 app.use("/jabatan", require("./apps/jabatan/JabatanControllers"));
 app.use("/golongan", require("./apps/golongan/GolonganControllers"));
-app.use("/gajidetail", require("./apps/gajidetail/GajiDetailControllers"));
 app.use("/gaji", require("./apps/gaji/GajiControllers"));
 module.exports = app;
